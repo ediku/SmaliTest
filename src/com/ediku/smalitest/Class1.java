703321c931c6d92576b82d4b31be65f89c4ec2d2
ediku <3935092@qq.com>
2015-09-20 19:28:01 +0800

    提交项目第一次

---
+++ src/com/ediku/smalitest/Class1.java
@@ -0,0 +1,22 @@
+package com.ediku.smalitest;
+
+public class Class1 {
+	 public String staticName;
+	 private String name;
+	      
+	 
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Class1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/ediku/smalitest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ediku.smalitest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		String a1="aa";
+		  String a2="bb";
+		  a1=a2;
+		  
+		  
+		  Class1 class1= new Class1();
+		  class1.staticName="ediku2";
+		   String b4=class1.staticName;
+		   
+		   
+		   class1.setName("ediku");
+		   String b3=class1.getName();
+		   
+		   
+	}
+ 
+}
